e8f9d56e5dac181159a32fd93d7d01b7343d4e08
Renske de Wit <h.c.de.wit@student.vu.nl>
2022-05-28 12:44:51 +0200

    Wrapped DSSP (SA calculation) in CWL

---
+++ tools/dssp.cwl
@@ -0,0 +1,86 @@
+#!/usr/bin/env cwl-runner
+
+cwlVersion: v1.2 
+class: CommandLineTool
+baseCommand: python3
+
+hints:
+  DockerRequirement:
+    dockerPull: biopython/biopython
+  SoftwareRequirement:
+    packages:
+      pandas:
+        version: [ "0.19.1" ]
+        specs: [ https://pypi.org/project/pandas/ ]
+      biopython:
+        specs: [ https://pypi.org/project/biopython/ ]
+      dssp:
+        specs: [ https://swift.cmbi.umcn.nl/gv/dssp/ ]
+        version: [ "2.0.4" ] # this version does not support mmCIF files
+
+arguments:
+- $(inputs.script.path)
+- $(inputs.source_dir.path)
+- "-o"
+- $(inputs.output_dir)
+- "-d"
+- $(inputs.dssp)
+- "-c"
+- $(inputs.rsa_cutoff)
+
+inputs:
+  script:
+    type: File
+    default: 
+      class: File
+      location: ./dssp_RASA.py 
+  source_dir:
+    type: Directory
+    default: # for testing purposes, remove this later!
+      class: Directory
+      location: ./dssp_data
+  output_dir:
+    type: string
+    default: "./dssp_scores"
+  dssp:
+    type: string
+    default: "dssp"
+  rsa_cutoff:
+    type: string
+    default: "0.06"
+
+outputs:
+  dssp_output:
+    type: Directory
+    outputBinding:
+      glob: $(inputs.output_dir)
+
+s:author:
+- class: s:Person
+  s:name: "Renske de Wit"
+s:license: <?>
+s:dateCreated: "2022-05-28"
+s:mainEntity:
+  class: s:SoftwareApplication
+  s:name: "dssp_RASA.py"
+  s:programmingLanguage: Python
+  s:license: <?>
+  s:author:
+  - class: s:Person
+    s:name: "DS"
+  s:about: "Script which takes a directory of pdb files as input and calculates relative surface accessibility for each residue in the protein sequence."
+  s:basedOn:
+  - class: s:SoftwareApplication
+    s:name: "DSSP"
+  
+$namespaces:
+  s: https://schema.org/
+  edam: http://edamontology.org/
+
+$schemas:
+- https://schema.org/version/latest/schemaorg-current-https.rdf
+- https://edamontology.org/EDAM_1.25.owl
+
+
+
+
---
+++ tools/dssp_RASA.py
@@ -0,0 +1,93 @@
+"""
+This script uses BioPython & DSSP to calculate surface accessiblity and secondary structure.
+
+@author: DS
+
+"""
+
+from glob import glob
+from os.path import join
+import argparse
+from pathlib import Path
+import pandas as pd
+from os import getcwd
+import logging
+from Bio.PDB import PDBParser
+from Bio.PDB.DSSP import DSSP
+
+
+"""
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+
+def get_file_list(dir, ext=".pdb"):
+    
+    files_path = join(dir, "*"+ ext)
+    files = glob(files_path)
+    return files
+
+def calculate_rsa(filename, rsa_cutoff, dssp_algo, output_dir):
+    """Returns a list of surface exposed residues as determined by relative solvent accessibility.
+    Parameters
+    ---------
+    filename: str
+        Name of pdb file to be analyzed
+    model: :class:`Bio.PDB.Model.Model`
+        Model under analysis
+    rsa_cutoff: float
+        Cutoff for buried/surface exposed
+    """
+    p = PDBParser()
+
+    name = filename.split('/')[-1]
+    name = name.split('.')[0]
+
+    structure = p.get_structure(name.upper(), filename)
+    model = structure[0]
+    
+    surface_exposed_res = []
+    surface_exposed_score = []
+    amino_acid = []
+    try:
+        dssp = DSSP(model, filename, dssp=dssp_algo)
+        for key in dssp.keys():
+            
+            if  dssp[key][3] >= rsa_cutoff:
+                surface_exposed_res.append(1)
+            else:
+                surface_exposed_res.append(0)
+                
+            surface_exposed_score.append(dssp[key][3])
+            amino_acid.append(dssp[key][1])
+
+        df = pd.DataFrame({"amino_acid": amino_acid, "RASA_score": surface_exposed_score, "RASA_bool": surface_exposed_res})
+
+        dest = join(output_dir, name+'_RASA.csv')
+        df.to_csv(dest, index=False)
+
+    except Exception:
+        warnings.warn("Unable to calculate solvent accessibility. Check that DSSP is installed. Omitting " +filename,
+                      RuntimeWarning,
+                      stacklevel=2)
+    
+def main(args):
+    source = args.source
+    output = args.output
+
+    files = get_file_list(source)
+    Path(output).mkdir(parents=True, exist_ok=True)
+    for file in files:
+        calculate_rsa(filename=file,rsa_cutoff=args.cutoff,dssp_algo=args.dssp,output_dir=output)
+
+        
+    return 0
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=str, help = "path to directory containing pdb files with extension .pdb")
+    parser.add_argument("--output",'-o', type=str, help = "Destinition  for output (default is current directory + dssp_scores)", default = join(getcwd(), 'dssp_scores'))
+    parser.add_argument("--dssp", "-d", type=str, help="DSSP algorothim to use, default = mkdssp", default = "mkdssp")
+    parser.add_argument("--cutoff", "-c", type=float, help = "cutoff value for accesibility bool for Relative Accesible Surface Area, defaut = 0.06", default = 0.06)
+    args = parser.parse_args()
+    main(args)
